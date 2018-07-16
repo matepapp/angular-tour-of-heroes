e96417defdd5b9b7414aa798a0a40dd54ed8e37c
Mate Papp <matepapp@icloud.com>
2018-07-16 18:24:47 +0200

    Implement routing with master-detail view

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { Hero } from "./hero";
-import { HEROES } from "./mock-heroes";
 import { MessageService } from "./message.service";
+import { HEROES } from "./mock-heroes";
 
 @Injectable({
   providedIn: "root"
 })
 export class HeroService {
   constructor(private messageService: MessageService) {}
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,16 +9,11 @@ import { HeroService } from "../hero.service";
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) {}
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit() {
